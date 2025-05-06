1f516ddc0a62f0419033681bf42ed7dc197c724d
dehazecontrast <andylecontact@gmail.com>
2025-05-06 15:26:21 -0400

    added grid component

---
+++ stoic-calendar/frontend/src/components/Grid/grid.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Grid.css'
+
+//generate a grid of squares empty and nonempty depending on duration lived
+export default function Grid ({}) {
+    let rows = 64;
+    let columns = 52;
+    let weekCounter = 0;
+
+    //create a 
+    for (let i = 0; i < rows; i++) {
+
+        for (let j = 0; j < columns; j++) {
+            
+            // //generate a filled/empty square depending on if weekCounter has met durationLived yet
+            // if (weekCounter <= durationLived) {
+
+            // }
+            // else {
+
+            // }
+            
+        }
+    }
+    
+    //loop for 18 y axes 
+
+    //drop down one square
+
+    //place a square 52 times
+}
